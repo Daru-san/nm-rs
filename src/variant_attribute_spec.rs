810ab932630396f221b8076198a533a6b0baa35c
Daru <mica.tui@proton.me>
2025-12-08 12:25:09 +0200

    fix: unchecked cast NMVariantAttributeSpec

--- src/variant_attribute_spec.rs
+++ src/variant_attribute_spec.rs
@@ -3,11 +3,12 @@ use glib::translate::FromGlibPtrNone;
 use crate::ffi;
 use std::ptr;
 
+#[allow(dead_code)]
 pub struct VariantAttributeSpec(ptr::NonNull<ffi::NMVariantAttributeSpec>);
 
 impl FromGlibPtrNone<*const *const ffi::NMVariantAttributeSpec> for VariantAttributeSpec {
     unsafe fn from_glib_none(ptr: *const *const ffi::NMVariantAttributeSpec) -> Self {
         let ptr = std::ptr::read(ptr);
-        Self(ptr::NonNull::new(ptr.cast_mut()))
+        Self(ptr::NonNull::new_unchecked(ptr.cast_mut()))
     }
 }
